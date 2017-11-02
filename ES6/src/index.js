7e643f48fb3eba4f2f2eaf86de4ae45057084d21
baojintong <123488257@qq.com>
2017-11-02 15:01:56 +0800

    111

--- ES6/src/index.js
+++ ES6/src/index.js
@@ -1,19 +1,9 @@
-const fs = require('fs');
-
-const readFile = function (fileName) {
-  return new Promise(function (resolve, reject) {
-    fs.readFile(fileName, function(error, data) {
-      if (error) return reject(error);
-      resolve(data);
-    });
-  });
-};
-
-const asyncReadFile = async function () {
-  const f1 = await readFile('a.txt');
-  const f2 = await readFile('b.txt');
-  //console.log(f1.toString());
-  //console.log(f2.toString());
-};
-
-asyncReadFile().then(x=>{console.log(x)})
+let a=[1,2,3];
+let b={
+  name:'111',
+  age:'222',
+}
+let c={
+  ...b
+}
+console.log(c.name);
